b9073b52df74011168556b44865b05de68efd16e
s3927562 <s3927562@rmit.edu.vn>
2023-08-26 15:44:39 +0700

    Initial Commit

---
+++ Battleship/BattleshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleshipApp.swift
+//  Battleship
+//
+//  Created by Tung Tran Thanh on 26/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BattleshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
